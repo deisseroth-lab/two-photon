ff05cec4f108a26ae86fb30d5bd783d746300784
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2021-05-26 04:52:46 -0700

    Setup suite2p params in external file - Check-in Tony's defaults

--- two_photon/analyze.py
+++ two_photon/analyze.py
@@ -17,7 +17,8 @@ logger = logging.getLogger(__name__)
     multiple=True,
     help="Additional acquisitions to include in analysis in addition to --acquisitions",
 )
-def analyze(layout, extra_acquisitions):
+@click.option("--suite2p-params-file", help="Optional Suite2p ops file (json format) to specify non-default options.")
+def analyze(layout, extra_acquisitions, suite2p_params_file):
     """Runs suite2p on preprocessed data."""
     preprocess_path = layout.path("preprocess")
     analyze_path = layout.path("analyze")
@@ -28,38 +29,29 @@ def analyze(layout, extra_acquisitions):
     data_paths = [preprocess_path] + [layout.path("preprocess", acq) for acq in extra_acquisitions]
     data_paths = [p / "preprocess" for p in data_paths]
 
+    # Use first file to determine the sampling rate.
+    with h5py.File(data_paths[0] / "preprocess.h5", "r") as h5_file:
+        z_planes = h5_file["data"].shape[1]
     period = utils.frame_period(layout)
-
-    z_planes = 0
-    for data_path in data_paths:
-        with h5py.File(data_path / "preprocess.h5", "r") as h5_file:
-            z_planes += h5_file["data"].shape[1]
-
     fs_param = 1.0 / (period * z_planes)
 
     # Use strings for paths: Suite2p and JSON cannot interpret Path, and logging is clearer.
     data_paths_str = [str(p) for p in data_paths]
 
-    # Load suite2p only right before use, as it has a long load time.
-    import suite2p
-
-    default_ops = suite2p.default_ops()
-    params = {
+    params_internal = {
         "input_format": "h5",
         "data_path": data_paths_str,
         "save_path0": str(analyze_path),
-        "nplanes": z_planes,
         "fs": fs_param,
-        "save_mat": True,
-        "bidi_corrected": True,
-        "spatial_hp": 50,
-        "sparse_mode": False,
-        "threshold_scaling": 3,
-        "diameter": 6,
     }
+    params_external = json.load(open(suite2p_params_file, "r")) if suite2p_params_file else {}
+    params = {**params_internal, **params_external}
 
     with open(json_path, "w") as fout:
         json.dump(data_paths_str, fout, indent=4)
     logger.info("Running suite2p on files:\n%s\n%s", "\n".join(data_paths_str), params)
 
-    suite2p.run_s2p(ops=default_ops, db=params)
+    # Load suite2p only right before use, as it has a long load time.
+    import suite2p
+
+    suite2p.run_s2p(params)
